8cc86eb93a72ae9585e4fff0701776839e7ee04a
Saif Ullah <saif.ullah@fuelbuddy.in>
2025-07-07 17:37:59 +0530

    added chevron

--- src/renderer/src/views/layouts/PillLayout.tsx
+++ src/renderer/src/views/layouts/PillLayout.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 // icons
 import DragHandle from '@/assets/icons/drag-handle.svg?react'
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
 
 // types
 type PillLayoutProps = {
@@ -18,11 +19,12 @@ const PillLayout: React.FC<PillLayoutProps> = ({ children }) => {
       <div id="drag-handle" className="flex items-center justify-center">
         <DragHandle className="w-5 h-5" />
       </div>
-      <div
-        className="w-full h-full flex flex-col gap-y-[15px] overflow-y-scroll py-2 px-1 hide-scrollbar"
-      >
+      <div className="w-full h-full flex flex-col gap-y-5 overflow-y-scroll py-2 px-1 hide-scrollbar">
         {children}
       </div>
+      <button id="pill-expand-button" className="w-full flex items-center justify-center py-0.5">
+        <ChevronDownIcon className="w-4 h-4" />
+      </button>
     </div>
   )
 }
